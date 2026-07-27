070208754c690a4a3d1414c98e2436630d975674
evrencirpici77 <evrencirpici77@gmail.com>
2026-07-27 05:12:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Supabase proje bilgilerini buraya yazın.
 window.GIZ_CONFIG = {
   SUPABASE_URL: "https://wtgnuftzafzkfrkkazyt.supabase.co"
-SUPABASE_ANON_KEY:sb_publishable_V6QB0Q3HlP8j6UnTt8_bPQ_XcjEyKlQ
+SUPABASE_ANON_KEY:"sb_publishable_V6QB0Q3HlP8j6UnTt8_bPQ_XcjEyKlQ"
 };
